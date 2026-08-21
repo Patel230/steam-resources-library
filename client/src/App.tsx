c4cb849c3a595e3a9482e1b2b6694ae4e596587e
Lakshman Patel <Lakshmanp230@gmail.com>
2026-08-21 03:52:06 +0000

    fix: honor GitHub Pages subpath in wouter routing (404 on index)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import CountryCoverage from "./pages/CountryCoverage";
 import ReviewQueue from "./pages/ReviewQueue";
-function Router() {
+
+function AppRoutes() {
   // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
@@ -27,7 +28,9 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <WouterRouter base={import.meta.env.BASE_URL || "/"}>
+            <AppRoutes />
+          </WouterRouter>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
